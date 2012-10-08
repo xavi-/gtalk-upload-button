be1edb6bdf053950851d3bb679847910ece718ff
Xavi <xavi.rmz@gmail.com>
2012-10-08 02:10:46 -0700

    Rearrange code to make it more readable and more robust.

--- app.js
+++ app.js
@@ -1,48 +1,16 @@
-document.getElementById("jqueryui-gtu").addEventListener("load", function() {
-	if(!window.jQuery) { return; }
-
-	window.gtu = window.gtu || {};
-	var $ = window.gtu.$ = window.jQuery.noConflict(true);
-
+(function(window, document) {
 	var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
 
-	new MutationObserver(function(mutations, obs) {
-		if($(".dw .no").length <= 0) { return; }
+	var $, $upload;
 
-		var newChatObs = new MutationObserver(function(mutations) {
-			mutations
-				.filter(function(m) { return m.addedNodes.length > 0; })
-				.forEach(function(m) {
-					$(m.addedNodes).find(".gy")
-						.css("position", "relative")
-						.append(
-							$("<button><span>⇪</span></button>")
-								.addClass("gtu-button-upload")
-								.css({
-									position: "absolute",
-									top: 2,
-									left: 0,
-									width: 15,
-									height: 18,
-									background: "#0FF",
-									padding: 0
-								})
-								.find("span")
-									.css({position: "relative", "top": -2 })
-								.end()
-						)
-					;
-				})
-			;
-		});
-
-		var $upload = $("<form/>")
+	function createUploadDialog() {
+		return $("<form/>")
 			.append("<label>File: <input type='file' /></label>")
 			.append("<br/>")
 			.append("<input type='submit' />")
 			.submit(function() {
 				var files = $("label > input", this)[0].files;
-				
+
 				var data = new FormData();
 				data.append("image", files[0]);
 				data.append("key", "449e0ea885033eea12558ae9ea945b52");
@@ -76,17 +44,67 @@ document.getElementById("jqueryui-gtu").addEventListener("load", function() {
 				open: function() { $("label > input", this).replaceWith("<input type='file' />"); }
 			})
 		;
+	}
 
-		var $chatContainer = $(".dw .no").delegate(".gtu-button-upload", "click", function(e) {
+	function addUploadButtons(elems) {
+		$(elems).find(".gy")
+			.css("position", "relative")
+			.append(
+				$("<button><span>⇪</span></button>")
+					.addClass("gtu-button-upload")
+					.css({
+						position: "absolute",
+						top: 2,
+						left: 0,
+						width: 15,
+						height: 18,
+						background: "#0FF",
+						padding: 0
+					})
+					.find("span")
+						.css({ position: "relative", "top": -2 })
+					.end()
+			)
+		;
+	}
+	function attachNewChatObserver() {
+		var $chatContainer = $(".dw .no:first-child");
+
+		new MutationObserver(function(mutations) {
+			var additions = mutations
+				.filter(function(m) { return m.addedNodes.length > 0; })
+				.map(function(m) { return Array.prototype.slice.call(m.addedNodes, 0); })
+			;
+
+			addUploadButtons(Array.prototype.concat.apply([], additions));
+		}).observe($chatContainer[0], { childList: true });
+
+		$chatContainer.delegate(".gtu-button-upload", "click", function(e) {
 			e.preventDefault();
 
 			var $msg = $(e.target).closest("table").find("textarea");
 			$upload.data("message-box", $msg).dialog("open");
-		});
-
-		newChatObs.observe($chatContainer[0], { childList: true });
+		}).each(function() { addUploadButtons(this); });
 
 		console.log("Attached new chat detector");
-		obs.disconnect();
-	}).observe(document.body, { childList: true });
-});
+	}
+
+	document.getElementById("jqueryui-gtu").addEventListener("load", function() {
+		if(!window.jQuery) { return; }
+
+		$ = window.jQuery.noConflict(true);
+
+		if($(".dw .no:first-child").length > 0) { attachNewChatObserver(); }
+		else {
+			new MutationObserver(function(mutations, obs) {
+				if($(".dw .no:first-child").length <= 0) { return; }
+
+				attachNewChatObserver();
+
+				obs.disconnect();
+			}).observe(document.body, { childList: true });
+		}
+
+		$upload = createUploadDialog();
+	});
+})(window, document);
