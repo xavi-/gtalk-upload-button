5170c0d071091d8e7f09de7d30446fa0c0e187fa
Xavi <xavi.rmz@gmail.com>
2012-12-16 22:32:42 -0800

    Rearrange insert.js code to address gmail loading issues.

--- insert.js
+++ insert.js
@@ -3,27 +3,29 @@
 	jquerycss.rel = "stylesheet";
 	jquerycss.type = "text/css";
 	jquerycss.media = "screen";
-	document.head.appendChild(jquerycss);
 
 	var jqueryui = document.createElement("script");
 	jqueryui.id = "jqueryui-gtu";
 	jqueryui.async = true;
-	document.body.appendChild(jqueryui);
 	jqueryui.addEventListener("load", function() { jqueryui.parentNode.removeChild(jqueryui); });
+	document.body.appendChild(jqueryui);
 
 	var jquery = document.createElement("script");
 	jquery.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.js";
 	jquery.async = true;
-	document.body.appendChild(jquery);
 	jquery.addEventListener("load", function() {
 		jquery.parentNode.removeChild(jquery);
 		jqueryui.src = "https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.js";
-		jquerycss.href = "https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/themes/excite-bike/jquery-ui.css";
+		jqueryui.addEventListener("load", function() {
+			document.head.appendChild(jquerycss);
+			jquerycss.href = "https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/themes/excite-bike/jquery-ui.css";
+		});
 	});
+	document.body.appendChild(jquery);
 
 	var app = document.createElement("script");
 	app.src = chrome.extension.getURL("app.js");
 	app.async = true;
-	document.body.appendChild(app);
 	app.addEventListener("load", function() { app.parentNode.removeChild(app); });
+	document.body.appendChild(app);
 })();
